45f4656fecb9f5af65344a14bf337bfdb1f27c30
Matthieu Pomies <matthieu.pomies40@gmail.com>
2020-11-25 23:41:16 +0100

    Ajoutez des fichiers projet.

---
+++ CarAPI/CarRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarAPI
+{
+    public class CarRequest
+    {
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public int Power { get; set; }
+
+        
+    }
+}
---
+++ CarAPI/Controllers/CarController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace CarAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateCar([FromBody] CarRequest carRequest)
+        {
+
+            try
+            {
+                string filePath = @"C:\Users\Matthieu\source\repos\CarAPI\CarAPI\JsonData\Car.json";
+
+                // serialize JSON to a string and then write string to a file
+                System.IO.File.WriteAllText(filePath, JsonConvert.SerializeObject(carRequest));
+
+                // serialize JSON directly to a file
+                using (StreamWriter file = System.IO.File.CreateText(filePath))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    serializer.Serialize(file, carRequest);
+                }
+
+                return Ok("car created");
+            }
+            catch (Exception ex) when (ex != null)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
